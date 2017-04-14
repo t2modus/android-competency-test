a95ecc30a2ccd4d5f9e45ae3643700f67b292279
eldenskloss <eldenskloss@gmail.com>
2017-04-14 16:23:21 -0500

    Added more detail to friend detail page

--- t2eldencomptest/src/main/java/com/elden/t2eldencomptest/activities/friend_list/FriendDetailsActivity.java
+++ t2eldencomptest/src/main/java/com/elden/t2eldencomptest/activities/friend_list/FriendDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.elden.t2eldencomptest.activities.friend_list;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,8 +13,10 @@ import android.widget.Toast;
 import com.elden.t2eldencomptest.Extras;
 import com.elden.t2eldencomptest.R;
 import com.elden.t2eldencomptest.T2Application;
+import com.elden.t2eldencomptest.databinding.ActivityFriendDetailsBinding;
 import com.elden.t2eldencomptest.model.FriendDetails;
 import com.elden.t2eldencomptest.service_interfaces.FriendRetrofitInterface;
+import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,21 +25,19 @@ import retrofit2.Response;
 public class FriendDetailsActivity extends AppCompatActivity {
     public static final String TAG = FriendDetailsActivity.class.getSimpleName();
     FriendRetrofitInterface retrofitInterface = T2Application.retrofit.create(FriendRetrofitInterface.class);
-
+    ActivityFriendDetailsBinding friendDetailBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend_details);
+        friendDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_friend_details);
 
         int friendId = getIntent().getIntExtra(Extras.FRIEND_ID, -1);
 
-        Toast.makeText(this, "Here is the id: " + friendId, Toast.LENGTH_SHORT).show();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(FriendDetailsActivity.this, "WIP: Send Message to Friend", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -45,7 +46,9 @@ public class FriendDetailsActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<FriendDetails> call, Response<FriendDetails> response) {
                 if(response.isSuccessful()){
-                    Toast.makeText(FriendDetailsActivity.this, "First name is " + response.body().getFirstName(), Toast.LENGTH_SHORT).show();
+                    //// TODO: 4/14/2017 need image for when image call was unsuccesfull
+                    Picasso.with(FriendDetailsActivity.this).load(response.body().getImg()).into(friendDetailBinding.ivProfilePic);
+                    friendDetailBinding.setFriendDetail(response.body());
                 } else {
                     Log.d(TAG, "onResponse: Failure");
                 }
